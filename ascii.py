218921216cb66ce3e3dfc98430dcec122932d5d0
Krish Suchak <suchak.krish@gmail.com>
2022-06-30 20:42:27 -0400

    more progress

--- ascii.py
+++ ascii.py
@@ -41,43 +41,48 @@ ascii = " .'`,^:\";~-_+<>i!lI?/\|()1{}[]rcvunxzjftLCJUYXZO0Qoahkbdpqwm*WMB8&%$#@
 
 im = Image.open(ifile)
 raw = im.load()
-print(im.size)
 width, height = im.size[0], im.size[1]
 awidth = width // scale
 aheight = height // scale
-print(" ░▒▓█")
 
 
 numAscii = len(ascii)
 
 j = 0
+print(raw[1500, 1500])
 
 while j < height:
     line1 = ""
-    line2 = ""
     i = 0
     while i < width-awidth:
-        grayscale = [round(sum(raw[k, j][0:3]) / (255*3) *
-                           (len(ascii) - 1)) for k in range(i, i+awidth)]
+        grayscale = []
+        for k in range(i, i + awidth):
+            grayscale.append(round(sum(raw[k, j][0:3]) / (255*3) *
+                                   (len(ascii) - 1)))
         char = max(set(grayscale), key=grayscale.count)
         line1 += ascii[char]*(1+round(width/height))
 
-        char = round(sum(raw[i, j][0:3]) / (255*3) * (len(ascii)-1))
-        line2 += ascii[char]*(1+round(width/height))
-
         i += awidth
-    if j == 0:
 
-        new = " "*len(line1)
-        new = new[:(len(new)-1)//2-1] + "NEW" + new[(len(new)-1)//2+2:]
-        old = " "*len(line2)
-        old = old[:(len(old)-1)//2-1] + "OLD" + old[(len(old)-1)//2+2:]
-
-        print(new + " | " + old)
-        print("_"*len(line1) + "   " + "_"*len(line2))
-        print("")
-    print(line1 + " | " + line2)
+    print(line1)
     j += aheight
+
+
+# j = 0
+
+# while j < height:
+#     line2 = ""
+#     i = 0
+#     while i < width-awidth:
+#         grayscale = [round(sum(raw[k, j][0:3]) / (255*3) *
+#                            (len(ascii) - 1)) for k in range(i, i+awidth)]
+#         char = round(sum(raw[i, j][0:3]) / (255*3) * (len(ascii)-1))
+#         line2 += ascii[char]*(1+round(width/height))
+
+#         i += awidth
+
+#     print(line2)
+#     j += aheight
 
 
 # im.save("test.jpg")
