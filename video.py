f4d0934b45d0ce444b1a60c50dd31315cbeb0b24
Krish Suchak <suchak.krish@gmail.com>
2022-07-03 12:58:05 -0400

    done

--- video.py
+++ video.py
@@ -1,4 +1,7 @@
 import cv2
+from glitch_this import ImageGlitcher
+from PIL import Image
+import numpy as np
 
 # mix vids frame by frame
 # 10s full pic, 5s zoomed in
@@ -31,14 +34,47 @@ print(len(block_frames))
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 writer = cv2.VideoWriter(
-    '/mnt/c/Users/sucha/Videos/Captures/out.mp4',
+    '/mnt/c/Users/sucha/Videos/Captures/final1.mp4',
     fourcc, 26, (1920, 1080))
 
 
+res_frames = []
 for i in range(len(ascii_frames)):
-    writer.write(ascii_frames[i])
-    writer.write(block_frames[i])
+    if i > 100 and i < 200:
+        res_frames.append(ascii_frames[i])
+        res_frames.append(block_frames[i])
 
+res_frames += res_frames[::-1]
+
+
+# new_frames = []
+# for idx, frame in res_frames:
+#     if
+
+# res_frames = new_frames
+
+ig = ImageGlitcher()
+
+
+def glitch(frame):
+    img = Image.fromarray(frame)
+    ig.inputarr = np.asarray(img)
+    ig.outputarr = np.array(img)
+    ig.seed = None
+    # ig.img_mode = 'RGB'
+    ig.pixel_tuple_len = len(img.getbands())
+    ig.img_width, ig.img_height = img.size
+    ig.img_mode = img.mode
+    img = ig._ImageGlitcher__get_glitched_img(1.1, 1, False)
+    frame = np.asarray(img)
+    return frame
+
+
+for frame in res_frames:
+    frame = glitch(frame)
+    # frame = cv2.resize(frame, (854, 480), fx=0, fy=0,
+    #                    interpolation=cv2.INTER_CUBIC)
+    writer.write(frame)
 
 ascii.release()
 writer.release()
